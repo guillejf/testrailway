7cc3800bb656fc49844f432ac68ff80fcb929cab
guillejf <maildeguille@gmail.com>
2023-02-02 11:55:49 -0300

    v1

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.json({
+    port: process.env.PORT,
+    MODE: process.env.MODE,
+    MONGO_URL: process.env.MONGO_URL,
+  });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`);
+  console.log({
+    port: process.env.PORT,
+    MODE: process.env.MODE,
+    MONGO_URL: process.env.MONGO_URL,
+  });
 });
